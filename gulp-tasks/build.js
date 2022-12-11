f54bf2461374132b403f5f83efcb5b09774cd1a3
kameleoon-manushenkov <114591257+kameleoon-manushenkov@users.noreply.github.com>
2022-12-12 04:23:43 +1000

    Update build.js

--- gulp-tasks/build.js
+++ gulp-tasks/build.js
@@ -1,226 +1,117 @@
-const gulp = require('gulp');
-const gulpif = require('gulp-if');
+const gulp = require('gulp')
+const gulpif = require('gulp-if')
 
 // debugging
-const plumber = require('gulp-plumber');
+const plumber = require('gulp-plumber')
 
 // css
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const concatCss = require('gulp-concat-css');
+const sass = require('gulp-sass')(require('sass'))
+const autoprefixer = require('gulp-autoprefixer')
+const concatCss = require('gulp-concat-css')
 
 // javascript
-const rollupJson = require('rollup-plugin-json');
-const rollup = require('gulp-better-rollup');
-const resolve = require('rollup-plugin-node-resolve');
-const commonjs = require('rollup-plugin-commonjs');
-const ts = require('rollup-plugin-typescript2');
-const rename = require('gulp-rename');
+const rollupJson = require('rollup-plugin-json')
+const rollup = require('gulp-better-rollup')
+const resolve = require('rollup-plugin-node-resolve')
+const commonjs = require('rollup-plugin-commonjs')
+const ts = require('rollup-plugin-typescript2')
+const rename = require('gulp-rename')
 
 //minifier
-const terser = require('gulp-terser');
+const terser = require('gulp-terser')
 
-const fs = require('fs');
+const path = require('path')
 
-const argv = require('minimist')(process.argv.slice(2));
-
-const minifier = argv['minifier'];
-
-const slash = process.platform === 'win32' ? '\\' : '/';
+const argv = require('minimist')(process.argv.slice(2))
 
 function build() {
-    const path = findDeepDir();
-    let customerDir = '.' + slash;
+	const buildTargetPath = argv['build-target-path']
+	const buildOutputPath = argv['build-output-path']
 
-    if (argv['customer-id']) {
-        customerDir = findDir('.' + slash, argv['customer-id'], 'Customer Id');
-    } 
+	buildFile(buildTargetPath, buildOutputPath)
 
-    let buildPath = customerDir + '_built' + slash + argv['sitecode'] + slash;
-
-    if (argv['experiment-id'] || argv['personalization-id'] || argv['variation-id'] || argv['common'] || argv['targeting']) {
-        if (argv['experiment-id']) {
-            buildPath += 'experiments' + slash;
-            buildPath += argv['experiment-id'] + slash;
-        } else {
-            buildPath += 'personalizations' + slash;
-            buildPath += argv['personalization-id'] + slash;
-        }
-    }
-
-    if (argv['variation-id']) {
-        buildFiles(findFiles(path, argv['variation-id']), buildPath);
-    }
-
-    else if (argv['common']) {
-        buildFiles(findFiles(path, 'common.'), buildPath);
-    }
-
-    else if (argv['targeting']) {
-        buildFiles(findFiles(path, 'targeting.'), buildPath);
-    }
-
-    else if ((argv['experiment-id'] || argv['personalization-id']) && !argv['variation-id'] && !argv['common'] && !argv['targeting']) {
-        buildFiles(findFiles(path, /^[0-9]+/g), buildPath);
-        buildFiles(findFiles(path, 'common.'), buildPath);
-        buildFiles(findFiles(path, 'targeting.'), buildPath);
-    }
-
-    else if (argv['global']) {
-        buildPath += 'global' + slash;
-        buildFiles(findFiles(path, 'index.'), buildPath);
-    }
-    
-    else if (argv['template-id']) {
-        buildPath += "templates" + slash;
-        buildPath += argv['template-id'] + slash;
-        let templatePath = path;
-        templatePath += fs.readdirSync(path).filter((file) => {
-            if (file.indexOf(argv['template-id'] + "-") != -1) {
-
-                return true;
-            }
-            return false;
-        })[0];
-        templatePath += slash;
-        buildFiles(findFiles(templatePath, "index."), buildPath);
-    }
-    
-    return gulp.src('.', {allowEmpty: true});
+	return gulp.src('.', { allowEmpty: true })
 }
 
-function findFiles(base, contains) {
-    const file = fs.readdirSync(base).filter((file) => {
-        if (file.match(contains)) {
-            return true;
-        }
-        return false;
-    });
-    if (file.length > 0) {
-        return file.map(file => {
-            return {
-                path: base,
-                name: file,
-                nameWithoutExt: file.match(contains)[0].toString().replace('.', '')
-            };
-        });
-    }
-    return [];
+function buildFile(filePath, buildPath) {
+	if (filePath.endsWith('.ts')) return compileTS(filePath, buildPath)
+
+	if (filePath.endsWith('.js')) return compileJS(filePath, buildPath)
+
+	if (filePath.endsWith('.css') || filePath.endsWith('.scss'))
+		return compileCSS(filePath, buildPath)
 }
 
-function findDir(base, contains, errorString) {
-    const file = fs.readdirSync(base).find((file) => {
-        if (file.match(contains)) {
-            return true;
-        }
-        return false;
-    });
-    if (file) {
-        return file + slash;
-    } else {
-        throw new Error(`No directory found for ${errorString}: ${contains}`);
-    }
+function getRelativeBuildPath(absolutePath) {
+	const splitBuildPath = absolutePath.split(path.sep)
+	const outputPath = splitBuildPath.slice(
+		splitBuildPath.indexOf('_build'),
+		-1
+	)
+
+	return path.join(...outputPath)
 }
 
-function findDeepDir() {
-    let path = '.' + slash;
-    if (argv['customer-id']) {
-        path += findDir(path, argv['customer-id'], 'Customer Id');
-    }
-    path += findDir(path, argv['sitecode'], 'Site code');
-    if (argv['experiment-id']) {
-        path += findDir(path, 'experiments', 'experiments');
-        path += findDir(path, argv['experiment-id'], 'Experiment Id');
-    }
-    if (argv['personalization-id']) {
-        path += findDir(path, 'personalizations', 'personalizations');
-        path += findDir(path, argv['personalization-id'], 'Personalization Id');
-    }
-    if (argv['global']) {
-        path += findDir(path, 'global', 'Global');
-    }
-    if (argv['template-id']) {
-        path += findDir(path, 'templates', 'Templates');
-    }
-    return path;
+function compileTS(filePath, buildPath) {
+	return gulp
+		.src(filePath)
+		.pipe(plumber({}))
+		.pipe(
+			rollup(
+				{
+					plugins: [
+						resolve(),
+						ts({ noEmit: true }),
+						commonjs(),
+						rollupJson(),
+					],
+				},
+				{ format: 'iife', output: { extend: true } }
+			)
+		)
+		.pipe(plumber.stop())
+		.pipe(
+			rename((path) => {
+				path.extname = '.js'
+			})
+		)
+		.pipe(terser())
+		.pipe(gulp.dest(getRelativeBuildPath(buildPath)))
 }
 
+function compileJS(filePath, buildPath) {
+	const outPutParams = {
+		format: filePath.match('global') ? 'iife' : 'cjs',
+		strict: false,
+	}
 
-function buildFiles(files, buildPath) {
-    files.forEach((file) => {
-        if (file.name.indexOf('.ts') !== -1) {
-            return compileTS(file, buildPath);
-        }
-        if (file.name.indexOf('.js') !== -1) {
-            return compileJS(file, buildPath);
-        }
-        if (file.name.indexOf('.css') !== -1 || file.name.indexOf('.scss') !== -1) {
-            return compileCSS(file, buildPath);
-        }
-    });
+	return gulp
+		.src(filePath)
+		.pipe(plumber({}))
+		.pipe(
+			rollup(
+				{ plugins: [resolve(), commonjs(), rollupJson()] },
+				outPutParams
+			)
+		)
+		.pipe(plumber.stop())
+		.pipe(terser())
+		.pipe(gulp.dest(getRelativeBuildPath(buildPath)))
 }
 
-function compileTS(file, buildPath) {
-    let options = {};
-    if (file.name.match("targeting.ts$")) {
-        options.errorHandler = (error) => {
-            if (error.message.indexOf('A \'return\' statement can only be used within a function body.') === -1) {
-                throw new Error(error);
-            }
-        }
-    }
-    return gulp.src(file.path + file.name)
-        .pipe(plumber(options))
-        .pipe(rollup({ plugins: [resolve(), ts({ noEmit: true }), commonjs(), rollupJson()] }, { format: 'iife', output: {extend: true}}))
-        .pipe(plumber.stop())
-        .pipe(rename(function (path) {
-            path.extname = '.js';
-            path.basename = file.nameWithoutExt;
-            if (process.platform === 'win32') {
-                path.dirname = buildPath;
-            }
-        }))
-        .pipe(gulpif(minifier, terser()))
-        .pipe(gulp.dest(process.platform === 'win32' ? ('.' + slash) : buildPath));
+function compileCSS(filePath, buildPath) {
+	return gulp
+		.src(filePath)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({ grid: true, cascade: true }))
+		.pipe(concatCss(filePath))
+		.pipe(
+			rename((path) => {
+				path.extname = '.css'
+				path.dirname = ''
+			})
+		)
+		.pipe(gulp.dest(getRelativeBuildPath(buildPath)))
 }
 
-function compileJS(file, buildPath) {
-    const outPutParams = {
-        format: file.name.match('global') ? 'iife' : 'cjs',
-        strict: false
-    };
-    let options = {};
-    if (file.name.match("targeting.js$")) {
-        options.errorHandler = (error) => {
-            if (error.message.indexOf('\'return\' outside of function') === -1) {
-                throw new Error(error);
-            }
-        }
-    }
-    return gulp.src(file.path + file.name)
-        .pipe(plumber(options))
-        .pipe(rollup({ plugins: [resolve(), commonjs(), rollupJson()] }, outPutParams))
-        .pipe(plumber.stop())
-        .pipe(rename(function (path) {
-            path.basename = file.nameWithoutExt;
-            if (process.platform === 'win32') {
-                path.dirname = buildPath;
-            }
-        })) 
-        .pipe(gulpif(minifier, terser()))
-        .pipe(gulp.dest(process.platform === 'win32' ? ('.' + slash) : buildPath));
-}
-
-function compileCSS(file, buildPath) {
-    return gulp.src(file.path + file.name)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({ grid: true, cascade: true }))
-        .pipe(concatCss(file.name))
-        .pipe(rename(function (path) {
-            path.extname = '.css';
-            path.basename = file.nameWithoutExt;
-        }))
-        .pipe(gulp.dest(buildPath));
-}
-
-exports.build = build;
+exports.build = build
